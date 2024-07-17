a7ced5f1d0c3b2e1bebdc44951441a0168587fda
Sachin Tripathi <sachintripathi972000@gmail.com>
2024-07-18 02:58:16 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,8 @@ const upload = multer();
 router.post("/upload", upload.single("file"), imageController.uploadCSV);
 router.get("/status/:requestId", imageController.checkStatus);
 router.get("/download/:requestId", imageController.downloadCSV);
+router.get('/', (req, res) => {
+  res.send("Hello there");
+})
 
 module.exports = router;
